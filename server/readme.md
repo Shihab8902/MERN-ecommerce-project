a25c18e35e04c65471ba004bfaf20048b74f2bb9
Shihab Hasan <shihabbd018@gmail.com>
2024-03-15 03:21:08 +0600

    created express server and do required configuration

--- server/readme.md
+++ server/readme.md
@@ -1,3 +1,4 @@
 ## Server side steps
 
 1. Create and listen a express server. (done);
+2. Create test route and install morgan package (todo).
--- server/src/server.js
+++ server/src/server.js
@@ -2,21 +2,32 @@ require("dotenv").config();
 
 const express = require("express");
 const app = express();
+const morgan = require("morgan");
 
 
 const port = process.env.PORT || 9000;
 
 
 
+//Middlewares
+app.use(morgan("dev"));
 
+
+
+
+
+//Testing route
+app.get("/test", (req, res) => {
+    res.send({ message: "The server is up and running...." });
+});
 
 
 
 //Listen server and database connection
 const run = async () => {
     app.listen(port, () => {
-        console.log(`The server is running http://localhost:${port}`)
+        console.log(`The server is running http://localhost:${port}`);
     });
-}
+};
 
 run();
